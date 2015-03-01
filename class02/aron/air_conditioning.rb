f6c8cd8b83e30b6d2f8d514d1e2e776471510548
AAmbia Ambia <aronamb@yahoo.com>
2015-03-01 15:34:16 -0500

    first pull request

---
+++ class02/aron/air_conditioning.rb
@@ -0,0 +1,8 @@
+puts "What is the current temperature?"
+temperature = gets.chomp
+puts "Is the AC functional?"
+yes_or_no = gets.chomp 
+puts "What temperature do you want it be?"
+setting = gets.chomp
+
+
