ea7a4cc3d3e6d2791ed468f02e0dd895d76d015b
AAmbia Ambia <aronamb@yahoo.com>
2015-03-08 13:40:39 -0400

    Cleanup

--- class02/aron/air_conditioning.rb
+++ class02/aron/air_conditioning.rb
@@ -1,8 +1,28 @@
-puts "What is the current temperature?"
-temperature = gets.chomp
-puts "Is the AC functional?"
-yes_or_no = gets.chomp 
-puts "What temperature do you want it be?"
-setting = gets.chomp
 
+#Air conditioning user input system. 
+
+puts "What is the current room temperature in degrees Celcius?"
+temperature = gets.chomp
+
+answer = "Yes"
+puts "Is the AC functional? (Yes/No)" 
+answer = gets.chomp
+
+puts "What temperature is comfortable for you?"
+comfortable_temperature = gets.chomp
+
+if 
+	temperature > comfortable_temperature
+	puts "Turn on the A/C please"
+end
+
+if 
+	answer = "Yes" && temperature > comfortable_temperature
+	puts "Fix the A/C now! It's hot!"
+end
+ else 
+if 
+	answer = "Yes" && temperature < comfortable_temperature
+	puts "Fix the A/C whenever you have the chance...It's cool..."
+end
 
